61ecfd90fd7b294dafd0a7ea51f867a4482290c2
Oluwatobi Shokunbi (just1and0) <ayoshokz@gmail.com>
2020-07-21 22:22:27 +0100

    circle CI setup

--- Home.test.js
+++ Home.test.js
@@ -4,7 +4,7 @@ import renderer from "react-test-renderer";
 import Home from "./Home";
 
 describe("<Home />", () => {
-  it("has 1 child", () => {
+  it("has 2 child", () => {
     const tree = renderer.create(<Home />).toJSON();
     expect(tree.children.length).toBe(1);
   });
