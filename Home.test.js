82ae42785c34f112212cdc77bed03f38a73d795e
Oluwatobi Shokunbi (just1and0) <ayoshokz@gmail.com>
2020-07-21 22:37:10 +0100

    circle CI setup

--- Home.test.js
+++ Home.test.js
@@ -6,6 +6,6 @@ import Home from "./Home";
 describe("<Home />", () => {
   it("has 1 child", () => {
     const tree = renderer.create(<Home />).toJSON();
-    expect(tree.children.length).toBe(2);
+    expect(tree.children.length).toBe(1);
   });
 });
